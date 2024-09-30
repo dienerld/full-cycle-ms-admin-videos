cebf565b69eb11a6823b66c4e794e41714f6ef49
Diener Dornelas <diener.ld@outlook.com>
2024-09-30 20:47:50 -0300

    chore: ajustes simples

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,13 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+    }
+  }
 ];
