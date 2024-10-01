715d6faf3a293c960b9b424d6c3f9a5c5aa0cfdc
Diener Dornelas <diener.ld@outlook.com>
2024-09-30 22:20:44 -0300

    feat: inicia criação de repositorio com opções de paginação e filtros

--- src/shared/db/in-memory/in-memory.repository.ts
+++ src/shared/db/in-memory/in-memory.repository.ts
@@ -1,7 +1,7 @@
 import type { Entity } from '@/shared/domain/entity'
 import { NotFoundError } from '@/shared/domain/errors/not-found.error'
-import type { SearchParams } from '@/shared/domain/repository/search-params'
-import type { SearchResult } from '@/shared/domain/repository/search-result'
+import type { SearchParams, SortDirection } from '@/shared/domain/repository/search-params'
+import { SearchResult } from '@/shared/domain/repository/search-result'
 import type { ValueObject } from '@/shared/domain/value-objects/value-object'
 import type {
   IRepository,
@@ -43,12 +43,60 @@ export abstract class InMemoryRepository<E extends Entity, EntityId extends Valu
   abstract getEntity(): new (...args: unknown[]) => E
 }
 
-export abstract class InMemorySearchableRepository<E extends Entity, EntityId extends ValueObject>
+export abstract class InMemorySearchableRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    Filter = string,
+  >
   extends InMemoryRepository<E, EntityId>
-  implements ISearchableRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>
 {
   sortableFields: string[]
-  search(query: SearchParams<string>): Promise<SearchResult<Entity>> {
-    throw new Error('Method not implemented.')
+  async search(query: SearchParams<Filter>): Promise<SearchResult<E>> {
+    // filter
+    const itemsFiltered = await this.applyFilter(this.items, query.filter)
+    // sort
+    const itemsSorted = this.applySort(itemsFiltered, query.sortBy, query.sortDir)
+    // pagination
+    const itemsPaginated = this.applyPagination(itemsSorted, query.page, query.pageSize)
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsPaginated.length,
+      currentPage: query.page,
+      pageSize: query.pageSize,
+    })
+  }
+
+  protected abstract applyFilter(items: E[], filter: Filter | null): Promise<E[]>
+  protected applyPagination(items: E[], page: number, pageSize: number): E[] {
+    const start = (page - 1) * pageSize
+    const limit = start + pageSize
+    return items.slice(start, limit)
+  }
+
+  protected applySort(
+    items: E[],
+    sortBy: string | null,
+    sortDir: SortDirection | null,
+    customGetter?: (sort: string, item: E) => 0 | 1 | -1
+  ): E[] {
+    if (!sortBy || !this.sortableFields.includes(sortBy)) {
+      return items
+    }
+
+    return [...items].sort((a, b) => {
+      const aValue = customGetter ? customGetter(sortBy, a) : a[sortBy as keyof E]
+      const bValue = customGetter ? customGetter(sortBy, b) : b[sortBy as keyof E]
+
+      if (aValue < bValue) {
+        return sortDir === 'asc' ? -1 : 1
+      }
+
+      if (aValue > bValue) {
+        return sortDir === 'asc' ? 1 : -1
+      }
+      return 0
+    })
   }
 }
